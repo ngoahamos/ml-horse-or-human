6aea918d3ec04a0a04bed69efd3f8ecaf7c8740f
Amos Ngoah <lifesoftdev@gmail.com>
2023-11-08 09:59:39 +0800

    added predicting to the image

--- horse_or_human.py
+++ horse_or_human.py
@@ -5,7 +5,9 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import MaxPooling2D
+from keras.preprocessing import image
 import scipy
+import numpy as np
 
 import urllib.request
 import zipfile
@@ -26,11 +28,8 @@ train_generator = train_datagen.flow_from_directory(training_dir, batch_size=128
 validation_datagen = ImageDataGenerator(rescale=1/255)
 validation_generator = validation_datagen.flow_from_directory(validation_dir, batch_size=128, target_size=(300,300), class_mode = 'binary')
 
-initial_cov = Conv2D(16,(3,3), activation='relu', input_shape=(300,300,3))
-max_pooling = MaxPooling2D(2,2)
 
-conv32 = Conv2D(32,(3,3), activation='relu')
-conv64 = Conv2D(64,(3,3), activation='relu')
+max_pooling = MaxPooling2D(2,2)
 
 input_layer = Flatten()
 hidden_layer = Dense(512,activation='relu')
@@ -58,3 +57,18 @@ model = Sequential([
 model.compile(loss='binary_crossentropy', optimizer=tf.keras.optimizers.RMSprop(learning_rate=0.001), metrics=['accuracy'])
 model.summary()
 model.fit(train_generator, epochs=15, validation_data = validation_generator)
+
+img = image.load_img('test1.jpg', target_size=(300,300))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+
+image_tensor = np.vstack([x])
+clases = model.predict(image_tensor)
+
+print(clases)
+print(clases[0])
+
+if (clases[0] > 0.5):
+    print('test image is a human')
+else:
+    print("test image is a horse")
